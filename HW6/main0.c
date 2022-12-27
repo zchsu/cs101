5cfdd150318cd8b83406be5014369699082e88c1
B1129005 <113523539+zchsu@users.noreply.github.com>
2022-12-28 00:59:24 +0800

    Update main0.c

--- HW6/main0.c
+++ HW6/main0.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i = 10;
+    
+    if (( i & (i - 1)) == 0) {
+        printf("\t %d is true\n" , i);
+    } else {
+        printf("\t %d is false \n ", i);
+    }
+    return 0;
+}
 
