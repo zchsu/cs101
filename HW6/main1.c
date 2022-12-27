e43cdc9a7db7b4ec316529eabc7044f0c7ab1bde
B1129005 <113523539+zchsu@users.noreply.github.com>
2022-12-28 01:02:28 +0800

    Update main1.c

--- HW6/main1.c
+++ HW6/main1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(){
+    int i = 10;
+    
+    if (i % 15 == 0) {
+        printf("\t Love IU\n" );
+    } else if (i % 5 == 0) {
+        printf("\t IU\n" );
+    } else if (i % 3 == 0) {
+        printf("\t Love\n" );
+    } else {
+        printf("\t %d\n" ,i);
+    }
+    return 0;
+}
