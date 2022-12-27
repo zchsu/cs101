c8c577cd182f9995f8db0d74a3544279c1529d11
B1129005 <113523539+zchsu@users.noreply.github.com>
2022-12-28 02:22:17 +0800

    Update main3.c

--- HW7/main3.c
+++ HW7/main3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int x = 1;
+    int y = 1;
+    
+    while(x <= 9) {
+        printf("%d*%d=%d\t", x, y, x*y);
+        if(y < 9) {
+            y++;
+        } else {
+            y = 1;
+            x++;
+            printf("\n");
+        }
+    }
+    return 0;
+}
