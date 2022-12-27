c9278ca849a39d30fa4464ea3797d9ff36d17917
B1129005 <113523539+zchsu@users.noreply.github.com>
2022-12-28 01:03:36 +0800

    Update main3.c

--- HW6/main3.c
+++ HW6/main3.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int year = 2022;
+    
+    if ((year % 4)== 0 && (year % 100)!= 0 || (year % 400) == 0) {
+	printf("\t%d是閏年\n" , year);
+	} else {
+		printf("\t%d不是閏年\n" , year);
+	} 
+    return 0;
+}
 
