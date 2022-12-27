7b806f84ebac408a7386f0c316451945c85ea8c3
B1129005 <113523539+zchsu@users.noreply.github.com>
2022-12-28 01:02:53 +0800

    Update main2.c

--- HW6/main2.c
+++ HW6/main2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int i = 10;
+    
+    if (i == 0) {
+        printf("\t zero \n" );
+    } else if (i % 2 == 0) {
+        printf("\t even \n" );
+    } else {
+        printf("\t odd \n" );
+    }
+    return 0;
+}
 
