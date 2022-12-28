a52a515b3494b24cb065b248ac5e89a90599e321
B1129005 <113523539+zchsu@users.noreply.github.com>
2022-12-28 10:06:31 +0800

    Update main1.c

--- HW7/main1.c
+++ HW7/main1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int n = 7;
+   int spc = n;
+   
+   for(int i=1;i<=n;i++){
+       for(int k=spc;k>=1;k--){
+           printf(" ");
+       }
+       for(int j=1;j<=i;j++){
+           printf("%d ",i);
+       }
+       printf("\n");
+       spc--;
+   }
+    return 0;
+}
 
